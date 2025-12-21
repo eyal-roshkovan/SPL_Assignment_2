d77fcded32b058c62bdf739e7a76be57ca7e6849
NoaTamam <tamno@pot.bgu.ac.il>
2025-12-21 20:05:17 +0200

    finish vector class

--- LinearAlgebraEngine/src/main/java/memory/SharedVector.java
+++ LinearAlgebraEngine/src/main/java/memory/SharedVector.java
@@ -10,21 +10,28 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        if (vector == null)
+            throw new IllegalArgumentException("vector cant be null");
+        this.vector = vector;
+        this.orientation = orientation;
+
     }
 
     public double get(int index) {
-        // TODO: return element at index (read-locked)
-        return 0;
+        if (vector.length <= index || index < 0)
+            throw new IllegalArgumentException("index out of bounds");
+
+        return vector[index];
     }
 
     public int length() {
         // TODO: return vector length
-        return 0;
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+        return orientation;
     }
 
     public void writeLock() {
@@ -45,22 +52,50 @@ public class SharedVector {
 
     public void transpose() {
         // TODO: transpose vector
+        if(orientation == VectorOrientation.ROW_MAJOR)
+            orientation = VectorOrientation.COLUMN_MAJOR;
+        else
+            orientation = VectorOrientation.ROW_MAJOR;
     }
 
     public void add(SharedVector other) {
-        // TODO: add two vectors
+        if (length() != other.length())
+            throw new IllegalArgumentException("Given vector is not in the correct size");
+        for (int i = 0; i < length(); i++)
+            vector[i] = vector[i] + other.get(i);
     }
 
     public void negate() {
-        // TODO: negate vector
+        for (int i = 0; i < length(); i++)
+            vector[i] = vector[i] * -1;
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        if (length() != other.length())
+            throw new IllegalArgumentException("Given vector is not in the correct size");
+
+        if (orientation != VectorOrientation.ROW_MAJOR || other.getOrientation() != VectorOrientation.COLUMN_MAJOR)
+            throw new IllegalArgumentException("Given vector is not column based");
+
+        double output = 0;
+        for (int i = 0; i < length(); i++)
+            output += vector[i] * other.get(i);
+
+        return output;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
         // TODO: compute row-vector × matrix
+        if(orientation != VectorOrientation.ROW_MAJOR)
+            throw new IllegalArgumentException("This vector is not row based");
+        if(matrix.getOrientation() != VectorOrientation.COLUMN_MAJOR)
+            matrix.loadColumnMajor(matrix.readRowMajor());
+
+        double[] newVector = new double[matrix.length()];
+        for(int i =0 ; i < matrix.length(); i++)
+            newVector[i] = dot(matrix.get(i));
+
+        vector = newVector;
     }
 }

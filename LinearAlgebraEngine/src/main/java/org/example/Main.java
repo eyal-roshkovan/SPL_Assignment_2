de1532a96a8e25bdf326a457a74f422ec7a58d12
NoaTamam <tamno@pot.bgu.ac.il>
2025-12-21 17:39:19 +0200

    added code

--- LinearAlgebraEngine/src/main/java/org/example/Main.java
+++ LinearAlgebraEngine/src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // to see how IntelliJ IDEA suggests fixing it`.
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint

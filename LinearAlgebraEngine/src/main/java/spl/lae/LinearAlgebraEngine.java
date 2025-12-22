288fe49b2b4b33826f69ebab9fc40a5d7f17cc72
NoaTamam <tamno@pot.bgu.ac.il>
2025-12-22 15:22:34 +0200

    start the LAE

--- LinearAlgebraEngine/src/main/java/spl/lae/LinearAlgebraEngine.java
+++ LinearAlgebraEngine/src/main/java/spl/lae/LinearAlgebraEngine.java
@@ -4,6 +4,7 @@ import parser.*;
 import memory.*;
 import scheduling.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinearAlgebraEngine {
@@ -13,22 +14,82 @@ public class LinearAlgebraEngine {
     private TiredExecutor executor;
 
     public LinearAlgebraEngine(int numThreads) {
-        // TODO: create executor with given thread count
+        executor = new TiredExecutor(numThreads);
     }
 
     public ComputationNode run(ComputationNode computationRoot) {
         // TODO: resolve computation tree step by step until final matrix is produced
-        return null;
+        ComputationNode result = null;
+        // TODO: wait for clarification on the "only matrix" input
+        computationRoot.associativeNesting();
+        ComputationNode newNode = computationRoot.findResolvable();
+
+        while (newNode != null) {
+            loadAndCompute(newNode);
+            double[][] res = leftMatrix.readRowMajor();
+            newNode.resolve(res);
+
+            result = new ComputationNode(res);
+            newNode = computationRoot.findResolvable();
+        }
+
+    return result;
     }
 
     public void loadAndCompute(ComputationNode node) {
         // TODO: load operand matrices
         // TODO: create compute tasks & submit tasks to executor
+        ComputationNodeType type = node.getNodeType();
+        List<Runnable> tasks = null;
+        List<ComputationNode> children = node.getChildren();
+
+        double[][] matrix1 = children.get(0).getMatrix();
+        double[][] matrix2 = null;
+
+        leftMatrix.loadRowMajor(matrix1);
+
+        if(children.size() == 2)
+         matrix2 = children.get(1).getMatrix();
+
+        if (type == ComputationNodeType.MULTIPLY) {
+            rightMatrix.loadColumnMajor(matrix2);
+            tasks = createMultiplyTasks();
+        }
+        else if (type == ComputationNodeType.ADD) {
+            rightMatrix.loadRowMajor(matrix2);
+            tasks = createAddTasks();
+        }
+        else if (type == ComputationNodeType.NEGATE)
+            tasks = createNegateTasks();
+
+        else if (type == ComputationNodeType.TRANSPOSE)
+            tasks = createTransposeTasks();
+
+        this.executor.submitAll(tasks);
+
     }
 
     public List<Runnable> createAddTasks() {
         // TODO: return tasks that perform row-wise addition
-        return null;
+        int row = leftMatrix.getRowsCount();
+        int col = leftMatrix.getColsCount();
+        if(row != rightMatrix.getRowsCount() || col != rightMatrix.getColsCount())
+            throw new IllegalArgumentException("The dimensions are not compatible");
+
+        List<Runnable> tasks = new ArrayList<>();
+
+        for (int i = 0; i < row ; i++) {
+             int rowIndex = i;
+
+            tasks.add(() -> {
+                SharedVector v1 = leftMatrix.get(rowIndex);
+                SharedVector v2 = rightMatrix.get(rowIndex);
+
+                v1.add(v2);
+            });
+        }
+        return tasks;
+
     }
 
     public List<Runnable> createMultiplyTasks() {

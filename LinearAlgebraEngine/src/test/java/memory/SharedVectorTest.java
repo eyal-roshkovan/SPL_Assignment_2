de7c746da0df2f1280966b798f8719d306186fb9
Eyal Roshkovan <roshkova@post.bgu.ac.il>
2025-12-27 22:57:37 +0200

    added get testers

--- LinearAlgebraEngine/src/test/java/memory/SharedVectorTest.java
+++ LinearAlgebraEngine/src/test/java/memory/SharedVectorTest.java
@@ -2,15 +2,22 @@ package memory;
 
 import static org.junit.jupiter.api.Assertions.*;
 class SharedVectorTest {
+    double[] firstVector = {1, 2, 3, 4, 5};
+    double[] secondVector = {6, 7, 8, 9, 10};
+    double[] thirdVector = {11, 12, 13, 14, 15};
+    double[] fourthVector = {16, 17, 18, 19, 20, 21};
+
     SharedVector sharedVectorFirstObject;
     SharedVector sharedVectorSecondObject;
+    SharedVector sharedVectorThirdObject;
+    SharedVector sharedVectorFourthObject;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-        double[] firstVector = {1, 2, 3, 4, 5};
-        double[] secondVector = {6, 7, 8, 9, 10};
         sharedVectorFirstObject = new SharedVector(firstVector, VectorOrientation.ROW_MAJOR);
         sharedVectorSecondObject = new SharedVector(secondVector, VectorOrientation.ROW_MAJOR);
+        sharedVectorThirdObject = new SharedVector(thirdVector, VectorOrientation.COLUMN_MAJOR);
+        sharedVectorFourthObject = new SharedVector(fourthVector, VectorOrientation.ROW_MAJOR);
     }
 
     @org.junit.jupiter.api.AfterEach
@@ -19,6 +26,17 @@ class SharedVectorTest {
 
     @org.junit.jupiter.api.Test
     void get() {
+        for (int i = 0; i < firstVector.length; i++)
+            assertEquals(firstVector[i], sharedVectorFirstObject.get(i));
+
+        for (int i = 0; i < secondVector.length; i++)
+            assertEquals(secondVector[i], sharedVectorSecondObject.get(i));
+
+        for (int i = 0; i < thirdVector.length; i++)
+            assertEquals(thirdVector[i], sharedVectorThirdObject.get(i));
+
+        for (int i = 0; i < fourthVector.length; i++)
+            assertEquals(fourthVector[i], sharedVectorFourthObject.get(i));
     }
 
     @org.junit.jupiter.api.Test

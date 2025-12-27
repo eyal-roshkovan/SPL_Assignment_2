b2c75cef032caea2ed00bf38741f563971fa7af1
Eyal Roshkovan <roshkova@post.bgu.ac.il>
2025-12-27 23:12:10 +0200

    added add testers

--- LinearAlgebraEngine/src/test/java/memory/SharedVectorTest.java
+++ LinearAlgebraEngine/src/test/java/memory/SharedVectorTest.java
@@ -86,6 +86,13 @@ class SharedVectorTest {
 
     @org.junit.jupiter.api.Test
     void add() {
+        assertDoesNotThrow(() -> sharedVectorFirstObject.add(sharedVectorSecondObject));
+        for (int i = 0; i < firstVector.length; i++)
+            assertEquals(firstVector[i] + secondVector[i], sharedVectorFirstObject.get(i));
+
+        assertThrows(IllegalArgumentException.class, () -> sharedVectorFirstObject.add(sharedVectorThirdObject)); // not same orientation
+
+        assertThrows(IllegalArgumentException.class, () -> sharedVectorFirstObject.add(sharedVectorFourthObject)); // not same length
     }
 
     @org.junit.jupiter.api.Test

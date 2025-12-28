99b66531efee41849a806423cbd2d536d236dd75
Eyal Roshkovan <roshkova@post.bgu.ac.il>
2025-12-28 21:42:32 +0200

    added matrix test file

---
+++ LinearAlgebraEngine/src/test/java/memory/SharedMatrixTest.java
@@ -0,0 +1,37 @@
+package memory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SharedMatrixTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void loadRowMajor() {
+    }
+
+    @Test
+    void loadColumnMajor() {
+    }
+
+    @Test
+    void readRowMajor() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void getOrientation() {
+    }
+}
